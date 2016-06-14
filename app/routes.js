5e04ab943ad1d658a6591fadb780019e8ea92494
xdrt81y <xdrt81y@gmail.com>
2016-06-14 16:35:16 +0800

    修改Bug

--- app/routes.js
+++ app/routes.js
@@ -6,7 +6,7 @@ import Stats from './components/Stats';
 import Character from './components/Character';
 import CharacterList from './components/CharacterList';
 import AddCharacter from './components/AddCharacter';
-import Hello from './components/Hello'
+import Hello from './components/Hello';
 
 export default (
   <Route component={App}>
